919dbc02422acbc2e3b8d0d0cc7ff4de50d482d8
Abril Armida <101528260+AbrilArmida@users.noreply.github.com>
2022-04-17 15:03:47 -0300

    Update TP_1_Caso_1.m

--- TP_1_Caso_1.m
+++ TP_1_Caso_1.m
@@ -3,57 +3,55 @@ clear all; close all; clc
 pkg load control
 pkg load symbolic
 
-%TP1 - PUCHETA
-
 %Variables
-R=4.7e3; L=10e-6; C=100e-9; Vin=-12; Vant=0; 
-i=1; 
-
-%Matrices
-Mat_A=[-R/L  -1/L; 1/C  0]
-Autovalores = eig(Mat_A)
-Mat_B=[1/L  0]
-Mat_C=[R  0]
-
-%Tiempos
-t95=log(0.95)/Autovalores(1)
-t5=log(0.05)/Autovalores(2)
-
-ti=t95/10 %su h
-ts=t5*3 %su tf
-
-t=[]; 
-pasos=ts/ti  
-aux=0.001/ti; %VER TODA
+ti=7e-03; %Valor aproximado (necesito que sea entero) 
+ts=1e-09; %Valor aproximado (necesito que sea entero)
+muestras=ti/ts;
+t=linspace(0,ti,muestras);
+I=zeros(1,muestras);
+Vc=zeros(1,muestras);
+u=linspace(0,ti,muestras);
+R=4.7e3; L=10e-6; C=100e-9; vin=12;
 
 %Condiciones iniciales
-vc(1)=0; il(1)=0; u(1)=12;
-tant=1e-3;
+I(1)=0; Vc(1)=0; u(1)=vin;
 
-X0=[0 0]'; x=[0 0]';
-Vin=Vin*(-1);
+Mat_A=[-R/L  -1/L; 1/C  0];
+Mat_B=[1/L  0];
+Mat_C=[R  0];
 
-while(i<=10000000+1)
+Il(1)=0; Vcl(1)=0; 
+x=[I(1) Vc(1)]' ; 
+y(1)=0; 
+Xop=[0 0]'; %punto de operacion
+ii=0;
 
-  %Variables del sistema lineal
-  il(i)=x(1); vc(i)=x(2);
-  u(i)=Vin;
-  t(i)=i*ti;
+%Ciclo para generar los graficos
+for i=1:muestras-1
+  
+  %Cambio de signo de u cada 1ms
+  ii=ii+ts;
+    if(ii>=1e-3)
+      ii=0;
+      vin=vin*-1;
+    end
+  %Variables de sistema lineal
+  u(i)=vin;
+  xp=Mat_A*(x-Xop)+Mat_B*u(i);
+  x=x+xp*ts;
+  Y=Mat_C*x;
+  y(i+1)=Y(1);
+  Il(i+1)=x(1);
+  Vcl(i+1)=x(2);
 
-  %sistema lineal
-  xp=Mat_A*(x-X0) + Mat_B*u(i);
-  x=x+ti*xp;
-
-  if(t(i)>tant) %hago variar de 12 a -12 cada 1ms
-  Vin=Vin*(-1);
-  tant=tant+1e-3;
-  end 
-i=i+1
+  i %para poder ver por donde va la cuenta
 end
+figure(1)
 
-subplot(3,1,1);hold on;grid on; %Grafica i, Vc y la entrada
-plot(t,il,'r');title('IL , t');
-subplot(3,1,2);hold on;grid on;
-plot(t,vc,'r');title('Vc, t');
-subplot(3,1,3);hold on;grid on;
-plot(t,u,'r');title('U , t');
+%Grafica I, Vc y u
+subplot(3,1,1);%hold on;
+plot(t,Il,'b');title('Corriente , i_t');grid on;
+subplot(3,1,2);%hold on;
+plot(t,Vcl,'r');title('Tension Capacitor , Vc_t');grid on;
+subplot(3,1,3);%hold on;
+plot(t,u,'b');title(' Tension de Entrada, u_t');grid on;
